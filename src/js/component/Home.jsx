e9b97018c762035c9652906e56291b1d014e6ffd
Bianca Salcedo <biancadmsadf16@gmail.com>
2023-10-10 21:07:34 +0000

    commit3

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -1,50 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-
-
-
-// [ ]
 const Home = () => {
   const [inputValue, setInputValue] = useState("");
-  const [todos, setTodos]=useState([]);
+  const [todos, setTodos] = useState([]);
+
+  function handleTask(event) {
+    setInputValue(event.target.value);
+  }
+
+  function handleTaskList(event) {
+    if (event.key === "Enter") {
+      if (inputValue.trim() !== "") {
+        setTodos([...todos, inputValue.trim()]);
+        setInputValue("");
+      }
+    }
+  }
+
+  function lista (index) {
+    let filterTask = todos.filter((item, indexFilter) => index !== indexFilter);
+    setTodos(filterTask);
+  }
+
+  useEffect(() => {
+    // Update your todo list with an API call whenever todos change
+    fetch('https://playground.4geeks.com/apis/fake/todos/user/biancadmsa', {
+      method: "PUT",
+      body: JSON.stringify(todos),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(resp => resp.json())
+      .then(data => {
+        console.log("Todo list updated:", data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }, [todos]); // Run this effect whenever todos change
+
   return (
-  <div className ="container"> 
-  
-  <h1>My Todos</h1>
-  
+    <div className="container">
+      <h1>My Todos</h1>
       <ul>
         <li>
-          <input type="text"  maxlength="38"
-          onChange={(e)=> setInputValue(e.target.value)} 
-          value={inputValue} 
-          onKeyDown= {(e) => {
-          
-            if (e.key === "Enter"){
-              if(todos.length < 10){
-                if(inputValue.trim() !== ""){
-              setTodos(todos.concat(inputValue));
-              setInputValue("");
-                } else alert("ingresa un texto")
-            }
-         }}}
-          
-          placeholder="What needs to be done?"></input>
+          <input
+            type="text"
+            maxLength="38"
+            onChange={handleTask}
+            value={inputValue}
+            onKeyDown={handleTaskList}
+            placeholder="What needs to be done?"
+          />
         </li>
-        {todos.map((item, index)=>(
-        <li>
-          {item}{""}<i class="fa-solid fa-xmark" 
-          onClick={()=>
-          setTodos(
-            todos.filter(
-            (t, currentIndex)=> 
-            index != currentIndex
-            )
-          )
-        }></i>
-        </li>
-      ))}
+        {todos.map((item, index) => (
+          <li key={index}>
+            {item}
+            <span>
+              <i
+                className="fa-solid fa-xmark"
+                onClick={() => lista(index)}
+              ></i>
+            </span>
+          </li>
+        ))}
       </ul>
-  <div>{todos.length} Item</div>
-  </div>
-  )}
-  export default Home;
+      <div>{todos.length} Item</div>
+    </div>
+  );
+};
+
+export default Home;
